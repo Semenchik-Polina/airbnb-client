e9e4a24c9081a845c1daeda8263949f0611d832b
Anastasiya Zaikouskaya <A.Zaikouskaya@itechart-group.com>
2019-03-20 18:32:06 +0300

    Fix reducer

--- src/admin/reducers/index.js
+++ src/admin/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-import roomsReducer from './rooms';
 import hotelInfoReducer from './hotel-info';
 
 export default combineReducers({
-    rooms: roomsReducer,
     hotelInfo: hotelInfoReducer,
 });
