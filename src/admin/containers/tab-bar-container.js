877c1a3a79291bbaf3da1a694f48288678c7ebc2
Anastasiya Zaikouskaya <A.Zaikouskaya@itechart-group.com>
2019-04-08 10:12:51 +0300

    Fix roboto font

--- src/admin/containers/tab-bar-container.js
+++ src/admin/containers/tab-bar-container.js
@@ -5,12 +5,12 @@ import TabBar from '../components/tab-bar/tab-bar';
 
 export default connect((state) => {
     const {
-        mainInfo, roomTypes, services, photos,
+        mainInfo, roomTypes, services, photoTour,
     } = state.adminReducer.hotelInfo;
     const isMainInfoFilled = !_.isEmpty(mainInfo);
     const isRoomFormFilled = !_.isEmpty(roomTypes) && isMainInfoFilled;
     const isServiceFormFilled = !_.isEmpty(services) && isRoomFormFilled;
-    const isPhotoFormFilled = !_.isEmpty(photos) && isServiceFormFilled;
+    const isPhotoFormFilled = !_.isEmpty(photoTour) && isServiceFormFilled;
 
     return {
         isMainInfoFilled, isRoomFormFilled, isServiceFormFilled, isPhotoFormFilled,
