2b6ca1630cec9ff58ea93fed465129d4519e6f91
Anastasiya Zaikouskaya <A.Zaikouskaya@itechart-group.com>
2019-04-11 14:17:15 +0300

    Fix loader a little

--- src/shared/components/loader/loader.jsx
+++ src/shared/components/loader/loader.jsx
@@ -1,9 +1,9 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 
 import BeatLoader from 'react-spinners/BeatLoader';
 
 import './loader.scss';
-// className="hotel-page hotel-page__loader"
+
 const Loader = () => (
     <div className="loader">
         <BeatLoader sizeUnit="px" size={20} color="#2B9E86" loading />
